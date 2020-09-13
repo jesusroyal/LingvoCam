896b87ae3358cc3a2b0ec54e67ddc4347d0f99dd
Mike <m.shevelinski@ya.ru>
2020-09-13 12:55:15 +0300

    Initial Commit

---
+++ LingvoCam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LingvoCam
+//
+//  Created by Mike Shevelinsky on 13.09.2020.
+//  Copyright © 2020 Mike Shevelinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
