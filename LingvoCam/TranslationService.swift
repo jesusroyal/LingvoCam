38b99443c0caaf2df4038fac6778fd4b7c00bb4e
Mike <m.shevelinski@ya.ru>
2020-09-17 21:30:11 +0300

    Added translation service

---
+++ LingvoCam/TranslationService.swift
@@ -0,0 +1,36 @@
+//
+//  TranslationService.swift
+//  LingvoCam
+//
+//  Created by Mike Shevelinsky on 17.09.2020.
+//  Copyright © 2020 Mike Shevelinsky. All rights reserved.
+//
+
+import Foundation
+
+
+final class TranslationService {
+    
+    
+    static func translate(text: String, completion: @escaping (String) -> Void){
+        let sourceLang = "ru"
+        let targetLang = "en"
+        let convertedText = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed )!
+        
+        let rawUrl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=\(sourceLang)&tl=\(targetLang)&dt=t&q=\(convertedText)";
+        
+        let url = URL(string: rawUrl)
+        
+        let session = URLSession(configuration: .default)
+        
+        let dataTask = session.dataTask(with: url!) {data , response, error in
+            
+            let jsonData = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions()) as? [Any]
+            
+            let translatedWord = ((jsonData![0] as! [Any])[0] as! [Any])[0] as! String
+            
+            completion(translatedWord)
+        }
+        dataTask.resume()
+    }
+}
--- LingvoCam/ViewController.swift
+++ LingvoCam/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var outputText: UILabel!
+    
+    @IBAction func translateDidTap(_ sender: Any) {
+        guard let text = inputText.text else { return }
+        
+        TranslationService.translate(text: text) { (test) in
+            DispatchQueue.main.async {
+                self.outputText.text = test 
+            }
+        }
+        
+        
+    }
+    
 }
 
